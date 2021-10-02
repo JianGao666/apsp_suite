5019d67bd396a05760b9076a4505ebdf4528607d
JianGao666 <84207576+JianGao666@users.noreply.github.com>
2021-10-02 00:54:38 -0500

    Add files via upload

---
+++ floyd_naive/hls.tcl
@@ -0,0 +1,14 @@
+open_project floyd_naive_hls
+
+add_files floyd.c
+add_files -tb ../common/main.c
+add_files local_support.c
+
+set_top Floyd
+
+open_solution -reset solution
+set_part virtex7
+create_clock -period 10
+
+csynth_design
+cosim_design -rtl verilog -tool xsim -trace_level all
